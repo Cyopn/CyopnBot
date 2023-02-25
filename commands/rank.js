5557ca18ef4e527d9ce554cac870b1316a11cd7c
Cyopn <frijolesconcatsu@gmail.com>
2023-02-25 01:03:10 -0600

    Comandos
    Comandos para obtener nivel (level) y la tabla de clasificación en el servidor (rank)

--- commands/rank.js
+++ commands/rank.js
@@ -1,34 +1,58 @@
 const { EmbedBuilder } = require("discord.js");
 const db = require('megadb');
-let lvl_db = new db.crearDB("niveles");
-module.exports.run = async(client, message, args, player) => {
-    let miembro = message.mentions.users.first();
-    if (!miembro) {
-        if (!lvl_db.tiene(`${message.guild.id}`)) return message.channel.send('No hay usuarios en el rank');
-        let user = message.author;
-        if (!lvl_db.tiene(`${message.guild.id}.${message.author.id}`)) return message.channel.send('El usuario no tiene xp ni un nivel');
-        let { xp, nivel } = await lvl_db.obtener(`${message.guild.id}.${message.author.id}`)
-        let lvlup = 5 * (nivel ** 2) + 50 * nivel + 100
-        let embed = new EmbedBuilder()
-            .setColor(Math.floor(Math.random() * 16777214) + 1)
-            .setTitle(`Estadisticas de ${user}`)
-            .setDescription(`XP: ${ xp } /${lvlup}\n Nivel: ${nivel}`)
-            .setFooter({ text: 'CyopnBot' })
-            .setTimestamp()
-        message.reply({ embeds: [embed] })
-    } else {
-        if (!lvl_db.tiene(`${message.guild.id}`)) return message.channel.send('No hay usuarios en el rank');
-        if (!lvl_db.tiene(`${message.guild.id}.${miembro.id}`)) return message.channel.send('El usuario no tiene xp ni un nivel');
-        let { xp, nivel } = await lvl_db.obtener(`${message.guild.id}.${miembro.id}`)
-        let lvlup = 5 * (nivel ** 2) + 50 * nivel + 100
-        let embed = new EmbedBuilder()
-            .setColor(Math.floor(Math.random() * 16777214) + 1)
-            .setTitle(`Estadisticas de ${miembro}`)
-            .setDescription(`XP: ${ xp } /${lvlup}\n Nivel: ${nivel}`)
-            .setFooter({ text: 'CyopnBot' })
-            .setTimestamp()
-        message.reply({ embeds: [embed] })
+const lvldB = new db.crearDB({
+    nombre: 'dataLevel',
+    carpeta: './database'
+})
+
+module.exports.run = async (client, message, args, player) => {
+    const guild = message.guildId
+    let b = {}
+
+    let keys = await lvldB.keys(`${guild}`)
+    for (let i in keys) {
+        let val = await lvldB.get(`${guild}.${keys[i]}`)
+        b[keys[i]] = val
     }
+    let arregloX = [];
+    for (let x in b) {
+        arregloX.push({
+            id: x,
+            xp: b[x].xp,
+            level: b[x].level
+        });
+    }
+    arregloX.sort(function (a, b) {
+        if (a.level > b.level) {
+            return -1;
+        } else if (a.level < b.level) {
+            return 1;
+        } else {
+            if (a.xp > b.xp) {
+                return -1;
+            } else if (a.xp < b.xp) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    });
+    let msg = ""
+    let j = 0
+    arregloX.forEach(k => {
+        j += 1
+        if (j <= 10) {
+            msg += `${j}-. <@${k.id}> ~ Nivel: ${k.level} ~ Experiencia: ${k.xp} \n`
+        }
+    })
+    const g = await message.client.guilds.cache.get(guild)
+    let embed = new EmbedBuilder()
+        .setTitle(`Nivel`)
+        .setDescription(`Tabla de clasificacion en **${g.name}** \n${msg}`)
+        .setColor(Math.floor(Math.random() * 16777214) + 1)
+        .setFooter({ text: "CyopnBot" })
+        .setTimestamp();
+    message.reply({ embeds: [embed] });
 }
 module.exports.config = {
     name: "rank",
