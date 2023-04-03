81eba57af6c40ddd49d9b8c315c84b01715d4545
Cyopn <2021452176@teschi.edu.mx>
2023-04-03 15:14:50 -0600

    Comandos
    Nuevo comando para barajar las canciones

---
+++ commands/shuffle.js
@@ -0,0 +1,56 @@
+const { createEmbed } = require("../lib/functions");
+
+module.exports.run = async (client, message, args, player) => {
+  let voicechannel = message.member.voice.channel
+    ? message.member.voice.channel
+    : null;
+
+  const queue = player.nodes.get(voicechannel.guild.id);
+
+  if (voicechannel == null) {
+    embed = await createEmbed("Advertencia", "Debes Estar en un canal de voz.");
+    message.reply({ embeds: [embed] });
+  } else {
+    if (queue == undefined || queue.metadata.vc != voicechannel.id) {
+      if (queue == undefined) {
+        embed = await createEmbed(
+          "Advertencia",
+          "No se esta reproduciendo nada justo ahora"
+        );
+        message.reply({ embeds: [embed] });
+      } else {
+        embed = await createEmbed(
+          "Advertencia",
+          "Debes estar en el mismo canal de voz que yo."
+        );
+        message.reply({ embeds: [embed] });
+      }
+
+    } else {
+      if (!queue.node.isPlaying()) {
+        embed = await createEmbed(
+          "Advertencia",
+          "No se esta reproduciendo nada justo ahora"
+        );
+        message.reply({ embeds: [embed] });
+      } else {
+        try {
+         queue.tracks.shuffle();
+          message.react("✅");
+        } catch (e) {
+          embed = await createEmbed(
+            "Error",
+            "Ocurrio un error al intentar pausar, intenta de nuevo o contacta a soporte"
+          );
+          message.reply({ embeds: [embed] });
+          console.log(e)
+        }
+      }
+    }
+  }
+
+};
+module.exports.config = {
+  name: "shuffle",
+  aliases: ["sf"],
+};
